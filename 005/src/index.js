3b78d2a78eb06b4ae0c37baffbb255b82b0e34ef
MaurickThom <thomtwd@gmail.com>
2019-06-29 01:32:36 -0500

    Instalacion de html loaders y html plugin

---
+++ 005/src/index.js
@@ -0,0 +1,7 @@
+import {sum} from './sum'
+const ob = {
+    name:'thom',
+    age:21
+}
+console.log({ob})
+console.log(sum(1,2))
--- 005/webpack.config.js
+++ 005/webpack.config.js
@@ -1,3 +1,16 @@
 module.exports = {
-    
+    // los loaders se guardaran en modules
+    module:{
+        // aquí son las reglas que webpack debe de seguir para transformar y leer archivos de tal tipo
+        rules:[
+            //cada regla va a definirse en un objeto , un objeto para babel otro para css otro para interpretar js
+            {
+                test:/\.(js)$/,
+                exclude:/node_modules/,
+                use : {
+                    loader:'babel-loader'
+                }
+            }
+        ]
+    }
 }
